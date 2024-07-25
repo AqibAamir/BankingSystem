6cecfe7a9453864a20889aa27624897ac95028f2
AqibAamir <144609363+AqibAamir@users.noreply.github.com>
2024-07-25 12:47:14 +0100

    Update Banking.py
    Worked on the second part for my personal banking program

--- Banking.py
+++ Banking.py
@@ -37,3 +37,55 @@ class BankingSystem:
     previousbalance = self.balance
     print("\n") # white spaces
     self.balance = self.balance + amount
+
+print(amount,"has been deposited into account:",self.username)
+    print("Previous Balance:",previousbalance,"\nNew Balance:",self.balance)
+    print("\n") # white spaces
+
+  def sendmoney(self,receiver,amount):
+    print("\n") #white spaces
+    if self.balance >= amount:
+
+      previousbalance = self.balance
+      self.balance = self.balance - amount
+      receiver.balance = receiver.balance + amount
+
+      print(self.username," has sent", amount,"to",receiver.username)
+      print(self.username,"Info- \nPrevious Balance:",previousbalance,"\nNew Balance:",self.balance)
+      print("\n") #white spaces
+
+    else:
+      print("Insufficient balance to send money.")
+
+
+class ceo(BankingSystem):
+  
+  def __init__(self, username, password, balance,rank):
+    super().__init__(username,password,balance,rank)
+  
+  def set_balance(self,target,newbalance):
+    target.balance = newbalance
+    print(target.username,"balance has been set to:",newbalance)
+  
+  def deduct_balance(self,target,amount):
+    target.balance = target.balance - amount
+    print(self.username,"has deducted",amount,"from",target.username)
+  
+  def add_balance(self,target,amount):
+    target.balance = target.balance + amount
+    print(self.username,"has added",amount,"to",target.username)
+
+
+
+#objects
+
+
+#load data
+with open('bankdatabackup.txt') as f:
+  for line in f:
+    attributes = line.split(':')
+    fuser = attributes[0]
+    fpass = attributes[1]
+    fbalance = int(attributes[2])
+    frank = attributes[3].replace("\n", "")
+
